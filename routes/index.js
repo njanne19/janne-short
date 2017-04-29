2004e210d1578fdee3f4517c25986a8a73f50402
njanne19 <nickjanne7@gmail.com>
2017-04-29 11:40:55 -0400

    Setting up to use mongoose

---
+++ routes/index.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var cors = require('cors');
+var router = express.Router();
+router.use(bodyParser.json());
+router.use(cors());
+
+router.get('/', function (req, res, next) {
+  res.render('index');
+});
+
+
+//Database
+router.get('/new/:urlToShorten(*)', function(req, res, next) {
+
+
+
+
+  res.json({original_url:req.params.urlToShorten})
+});
+
+
+module.exports = router;
