a81f6a974142045441a04abf4ef2805b06c2c940
njanne19 <nickjanne7@gmail.com>
2017-05-01 00:17:31 -0400

    Ready for deployment

--- routes/index.js
+++ routes/index.js
@@ -20,7 +20,34 @@ router.get('/', function (req, res, next) {
 });
 
 
-//Database
+//Database Referencing
+router.get('/:urlToForward', function (req, res, next) {
+
+  var userSnippet = req.params.urlToForward;
+
+  short_url.findOne({'short_url' : userSnippet}, function (err, data) {
+     if (err) res.send("ERROR READING DATABASE/URL NOT FOUND");
+
+     var regEx2 = new RegExp("^(http|https)://", "i");
+
+     var toCheck = data.original_url;
+
+     if(regEx2.test(toCheck) == true) {
+       res.redirect(301, data.original_url);
+     }
+
+     else {
+       res.redirect(301, 'http://' + data.original_url);
+     }
+  });
+});
+
+
+
+
+
+
+//Database logging
 router.get('/new/:urlToShorten(*)', function(req, res, next) {
 
 var original_url = req.params.urlToShorten;
@@ -44,7 +71,7 @@ if(regEx.test(original_url) == true) {
       return res.send("Error with database");
     }
   });
-  res.json({data});
+  res.json({original_url : data.original_url, short_url: data.short_url});
 }
 return res.json({response: "Error invalid URL"});
 });
